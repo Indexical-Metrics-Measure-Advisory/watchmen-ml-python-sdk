789d44c05c1c4477240ad18dc7a6b23462115821
luke0623 <luke0623@outlook.com>
2022-05-25 19:40:20 +0800

    update

---
+++ ml/sdk/index.py
@@ -0,0 +1,27 @@
+
+
+class WatchmenMLSDK(object):
+    
+    def __init__(self,env):
+        pass
+
+    def login(self):
+        pass
+
+    def load_dataset(self,dataset_name:str,data_frame_type:str):
+        pass
+
+    def register_notebook(self):
+        pass
+
+    def register_ml_model(self):
+        pass
+
+
+
+
+
+
+
+
+
