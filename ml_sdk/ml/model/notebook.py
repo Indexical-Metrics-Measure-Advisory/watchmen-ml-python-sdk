acdb6d67aed94d718bd994f203020eb3159ea2f9
luke0623 <luke0623@outlook.com>
2022-08-04 01:07:23 +0800

    add streamlit sdk

--- ml_sdk/ml/model/notebook.py
+++ ml_sdk/ml/model/notebook.py
@@ -2,11 +2,8 @@ from pydantic import BaseModel
 
 
 class WatchmenNotebook(BaseModel):
-    name:str = None
-    storageType:str = None
-    storageLocation:str = None
-    environment:dict = {}
-    dependencies:dict = {}
-
-
-
+	name: str = None
+	storageType: str = None
+	storageLocation: str = None
+	environment: dict = {}
+	dependencies: dict = {}
